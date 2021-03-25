6442567d48fa8f88724f300f21990b101bbf2bd9
Sergey Afonchenko <49588154+DIRECTcut@users.noreply.github.com>
2021-03-26 00:49:50 +0300

    format router

--- src/server/api.router.ts
+++ src/server/api.router.ts
@@ -1,84 +1,35 @@
-import express    from 'express';
-import ytdl, { videoInfo }       from 'ytdl-core';
-import bodyParser from 'body-parser';
+import express from 'express';
+import ytdl from 'ytdl-core';
 
 const router = express.Router();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-router.post('/info', bodyParser.json(), async (req, res, next) => {
-    const videoID   = req.body.videoID
+router.post('/info', express.json(), async (req, res) => {
+    const videoID = req.body.videoID
     const isIDValid = ytdl.validateID(videoID)
 
     if (isIDValid) {
         const response: Promise<ytdl.videoInfo> = await ytdl.getInfo(videoID)
-            .then( (result: ytdl.videoInfo) => {
+            .then((result: ytdl.videoInfo) => {
                 res.json(result.formats);
             })
-            .catch( err => {
-                res.status(400).json({error: err.message});
+            .catch(err => {
+                res.status(400).json({ error: err.message });
                 return err;
             })
     } else {
-        res.status(400).json({error: `No video id found: ${videoID}`})
+        res.status(400).json({ error: `No video id found: ${videoID}` })
     }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-router.post('/download', bodyParser.json(), (req, res, next) => {
-    const stream  = ytdl(req.body.videoID, { quality: req.body.quality });
+router.post('/download', express.json(), (req, res, next) => {
+    const stream = ytdl(req.body.videoID, { quality: req.body.quality });
 
     res.setHeader('Content-Type', 'video/mp4');
     stream.pipe(res);
 
     stream.on('error', (err) => {
         res.removeHeader('Content-Type');
-        res.status(400).send({error: err.message});
+        res.status(400).send({ error: err.message });
         next();
     });
 });
